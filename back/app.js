3c4e4a90bd08b8391f154282ae8de9e6476772dd
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-11-06 18:10:24 +0100

    stripe

--- back/app.js
+++ back/app.js
@@ -3,9 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
 const mongoose = require('mongoose');
-const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
+const Publishable_Key = 'pk_test_51HkSdNDXZyAsNyKOasOLrBiGaAbgoxHb8WzeD1fMyxhcFHXEWOOnKIuUAb0GR9LB5h3BsVkEUB38RcNVvZrk8WOd00OfoO2PLu'
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const path = require('path');
 
 const app = express();
+
+// express static utilisé pour tester des pages statiques dans static_stripe_views
+app.use(express.static('static_stripe_views'));
 
 mongoose.connect(process.env.DATABASE,
   {
@@ -23,12 +28,46 @@ app.use((req, res, next) => {
   next();
 });
 
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
+
 app.use(bodyParser.json());
 
 app.use('/auth', userRoutes);
 
+// get pour home
+app.get('/', function (req, res) {
+  // res render 'home' renvoit vers une vue ejs dans le dossier views
+  res.render('home')
+})
 
 
+
+// test de paiement avec stripe   A CHANGER AVEC localhost 3500
+const YOUR_DOMAIN = 'http://localhost:3000';
+
+app.post('/create-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Stubborn Attachments',
+            images: ['https://i.imgur.com/EHyR2nP.png'],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+  });
+  res.json({ id: session.id });
+});
 
 
 module.exports = app;
