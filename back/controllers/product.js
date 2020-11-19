e3a1079af224a9b3d99e424036d34cd8bcaf8458
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-11-19 16:52:53 +0100

    stripe -paiement ok

--- back/controllers/product.js
+++ back/controllers/product.js
@@ -1,16 +1,34 @@
 const Product = require('../models/product');
 const User = require('../models/user');
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST);
+
+
+
 exports.getAll = async (req, res, next) => {
   const allProduct = await Product.find({});
-  console.log(allProduct);
   res.render('testproduct', {
     products: allProduct
   })
 };
 
 exports.get = async (req, res, next) => {
-  console.log(JSON.stringify(req.params));
   const product = await Product.findById({_id: req.params.id});
-  console.log(product);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.price,
+    currency: 'eur',
+    payment_method_types: ['card'],
+    description: `la description du produit : ${product.description}`,
+    statement_descriptor: `x1 ${product.name}`.substr(0,22),
+    metadata: {
+      product_id: product.id,
+      legreg: "bonjour"
+    }
+  });
+    res.render('productShow', {
+    client_secret: paymentIntent.client_secret,
+    product: product,
+    key: process.env.PUBLISHABLE_KEY
+  })
+
 };
