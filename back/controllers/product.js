f79eb50deea03b60be4b5de4eebf07fc66fa46fa
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-12-03 17:16:13 +0100

    ok

--- back/controllers/product.js
+++ back/controllers/product.js
@@ -13,26 +13,9 @@ const allProduct = catchAsync(async (req, res, next) => {
 
 // La methode get qui devrait être refacto afin de clairement assigner à chaque methode une fonction claire. Ici la méthode joue le double role d'afficher un produit et de créer une intention de paiment pour stripe. En chantier ##
 const userInfo = catchAsync(async (req, res, next) => {
-  console.log("le req body oneproduct" + req.body)
-  console.log(req.cookies.email);
-  const product = await Product.findById({ _id: req.params.id });
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: product.price,
-    currency: 'eur',
-    payment_method_types: ['card'],
-    description: `la description du produit : ${product.description}`,
-    statement_descriptor: `x1 ${product.name}`.substr(0, 22),
-    metadata: {
-      product_id: product.id,
-      legreg: "bonjour"
-    }
-  });
-  res.render('productShow', {
-    client_secret: paymentIntent.client_secret,
-    product: product,
-    key: process.env.PUBLISHABLE_KEY
-  })
+
+
 });
 
 module.exports = { allProduct, userInfo };
