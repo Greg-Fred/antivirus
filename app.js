e0c0c81a10b6c09d985c62736878c080e826f365
Frédéric Legrand <fedde.leg@gmail.com>
2021-01-09 17:33:27 +0100

    changement structure de dossier

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ const emailRoutes = require('./routes/email');
 
 //Configuration de push temporaire pour heroku
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('/front/build'));
+  app.use(express.static('/client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/back/build/index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   });
 }
 
